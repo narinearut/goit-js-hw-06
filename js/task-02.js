eded8be9abb2e27516489d9d771c0cec7e45485b
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-12 15:38:06 +0000

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const findIngredients = document.querySelector('#ingredients');
+
+
+const addItem = ingredients => {
+  return ingredients.map(el => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = el;
+
+  return itemEl;
+});
+};
+
+const ingredientsEl = addItem(ingredients);
+findIngredients.append(...ingredientsEl);
+
+console.log(findIngredients);
