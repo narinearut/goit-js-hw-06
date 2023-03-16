117fd71ba82e4a66e6b858fd244f2ae10c7daa83
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-16 20:08:23 +0000

    task 7

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
 
+const refs = {
+    form: document.querySelector(".login-form"),
+
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (e) {
+    e.preventDefault();
+
+    // console.dir(e.currentTarget.elements.email.value);
+
+    const formData = new FormData(e.currentTarget);
+
+    formData.forEach((value, name) => {
+
+        if(value.length === 0) {
+            return window.alert('Помилка! Всі поля повинні бути заповнені.');
+        }
+
+        formData[name] = value;
+        // console.log('Поле:', name);
+        // console.log('Значення:', value);
+        
+    });
+    
+    console.log(formData);
+
+    refs.form.reset();
+}
+
+
+
