4d254e9d06e771dad8e5cac6631dcacd08458f1e
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-16 14:29:25 +0000

    task 6

---
+++ js/task-06.js
@@ -0,0 +1,32 @@
+
+const validationInput = document.querySelector("#validation-input");
+
+validationInput.addEventListener("blur", (onInputBlur)); 
+
+function onInputBlur () {
+    const inputLength = validationInput.value.length;
+    console.log(inputLength);
+
+    inputLength >= 6 
+    ? onValidInput()
+    : onInvalidInput()
+}
+
+function onValidInput () {
+validationInput.classList.add('valid');
+validationInput.classList.remove('invalid');
+}
+
+function onInvalidInput () {
+    validationInput.classList.add('invalid');
+    validationInput.classList.remove('valid');
+}
+
+
+    // if (inputLength >= 6) {
+    //     validationInput.classList.add('valid');
+    //     validationInput.classList.remove('invalid'); //valid = green
+    // } else if(validationInput.value.length < 6) {
+    //     validationInput.classList.add('invalid');
+    //     validationInput.classList.remove('valid');//invalid = red
+    // };
