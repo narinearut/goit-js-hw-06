e4679a4ec95258c3404b6503cfa4cc37a40568a2
Narine <115227487+narinearut@users.noreply.github.com>
2023-03-16 20:14:59 +0000

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -24,7 +24,7 @@ function onFormSubmit (e) {
         // console.log('Значення:', value);
         
     });
-    
+
     console.log(formData);
 
     refs.form.reset();
@@ -32,3 +32,19 @@ function onFormSubmit (e) {
 
 
 
+// const formEl = document.querySelector(".login-form");
+// const handleCheckVerifyFormData = (event) => {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+//   const dataObj = {};
+//   if (email.value === "" || password.value === "") {
+//     return alert("Please fill in all the fields!");
+//   }
+//   dataObj.email = email.value;
+//   dataObj.password = password.value;
+//   console.log(dataObj);
+//   formEl.reset();
+// };
+// formEl.addEventListener("submit", handleCheckVerifyFormData);
